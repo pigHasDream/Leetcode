84148c88e2090deb4fe45fea6adb834e23f550de
Jin M <jinmiao@utexas.edu>
2019-04-07 12:21:37 -0700

    new template

---
+++ template_lower_upper_bound.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int upper_bound(const vector<int>& A, int val, int l, int r) {
+  while (l < r) {
+    int m = l + (r - l) / 2;
+    if (A[m] > val)
+      r = m;
+    else      
+      l = m + 1;
+  }
+  return l;
+}
+ 
+int lower_bound(const vector<int>& A, int val, int l, int r) {
+  while (l < r) {
+    int m = l + (r - l) / 2;
+    if (A[m] >= val)
+      r = m;
+    else
+      l = m + 1;
+  }
+  return l;
+}
+ 
+int main() {
+  vector<int> A{1, 2, 2, 2, 4, 4, 5};
+  cout << lower_bound(A, 0, 0, A.size()) << endl; // 0
+  cout << lower_bound(A, 2, 0, A.size()) << endl; // 1
+  cout << lower_bound(A, 3, 0, A.size()) << endl; // 4
+  cout << upper_bound(A, 2, 0, A.size()) << endl; // 4
+  cout << upper_bound(A, 4, 0, A.size()) << endl; // 6
+  cout << upper_bound(A, 5, 0, A.size()) << endl; // 7
+}
+
--- template_monotone_stack.cpp
+++ template_monotone_stack.cpp
@@ -17,3 +17,4 @@ void monotoneStack(vector<int>& A) {
 
   return;
 }
+
