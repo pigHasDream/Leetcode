2c7e48fe7731efb2b40499e4a93d192f3668c1a8
pigHasGitHub <jinmiao@utexas.edu>
2019-04-08 00:19:26 -0700

    Update template_binary_search.cpp

--- template_binary_search.cpp
+++ template_binary_search.cpp
@@ -1,13 +1,16 @@
 
 int doBinarySearch(int left, int right) {
+  // Find the min index to satisfy g(index) == true
+  // [left, right)
   
-  // closed in left, open in right [left, right);
   while(left < right) {
     const int mid = left + (right - left)/2;
 
-    if(f(mid)) return mid; // optional, find and return immediately just once by g(mid).
+    // optional, find and return immediately
+    if(f(mid)) return mid; 
 
-    if(g(mid) { // if satisfy, we continue to find the min one satisfying the g(mid)
+    // if satisfy g(mid), continue finding the min one
+    if(g(mid) { 
       // do the right [left, mid)
       right = mid;
     }
@@ -17,7 +20,6 @@ int doBinarySearch(int left, int right) {
     }
   }
 
-  return left // or not found if we use f(mid) already!;
+  return left // or not found
 }
-
-// Find the min index to satisfy g(index) ==true and return!!
+       
