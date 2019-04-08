53517884fff7b218e5eecaacbfefb09ee66b0839
pigHasGitHub <jinmiao@utexas.edu>
2019-04-08 00:23:14 -0700

    Update template_connected_components.cpp

--- template_connected_components.cpp
+++ template_connected_components.cpp
@@ -1,24 +1,33 @@
-class Solution {
+class Solution { 
 public:
+  
   int numComponents() {
-    unordered_map<int, vector<int>> g;
+    // count connected componets of graph
+    unordered_map<int, vector<int>> graph;
     int ans = 0;
 
     unordered_set<int> visited;
 
-    for (int u : G) {      
+    for (const auto& u : graph) {      
       if (visited.count(u)) continue;
       ++ans;
-      dfs(u, g, visited);
+      // DFS to mark visited.
+      dfs(u, graph, visited);
     }
+    
     return ans;
   }
-private:
 
-  void dfs(int cur, unordered_map<int, vector<int>>& g, unordered_set<int>& visited) {
+  void dfs(const int cur, 
+           const unordered_map<int, vector<int>>& graph, 
+           unordered_set<int>& visited) {
+    
     if (visited.count(cur)) return;
+    
     visited.insert(cur);
-    for (const int next : g[cur])
-      dfs(next, g, visited);    
+    
+    for (const auto& next : graph[cur])
+      dfs(next, graph, visited);  
+    
   }
 };
