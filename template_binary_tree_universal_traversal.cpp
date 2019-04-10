057dd945b1021df728c0b34f08f90433b5444c93
pigHasGitHub <jinmiao@utexas.edu>
2019-04-10 11:36:25 -0700

    Update template_binary_tree_universal_traversal.cpp

--- template_binary_tree_universal_traversal.cpp
+++ template_binary_tree_universal_traversal.cpp
@@ -1,1 +1,31 @@
 
+vector<int> postorderTraversal(TreeNode* root) {
+  vector<int> res;
+  stack<pair<TreeNode*, bool>> theSt;
+  
+  theSt.push({root, false});
+  
+  while(not theSt.empty()) {
+    auto rt = theSt.top().first;
+    auto visit = theSt.top().second;
+    theSt.pop();
+    
+    if(rt == nullptr)
+      continue;
+    
+    if(visit) {
+      res.emplace_back(rt->val);
+    }
+    else {
+      // reversely push the nodes
+      // post order: root, right, left
+      // in order: right, root, left
+      // pre order: right, left, root
+      theSt.push({rt, true});
+      theSt.push({rt->right, false});
+      theSt.push({rt->left, false});
+    }
+  }
+  
+  return res;
+}
