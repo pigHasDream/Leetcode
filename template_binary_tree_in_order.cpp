37d1a334a28e3e97b706ac028b72fa13f7950b56
pigHasGitHub <jinmiao@utexas.edu>
2019-04-10 11:18:54 -0700

    Update template_binary_tree_in_order.cpp

--- template_binary_tree_in_order.cpp
+++ template_binary_tree_in_order.cpp
@@ -1,1 +1,20 @@
 
+vector<int> inorderTraversal(TreeNode* root) {
+  vector<int> res;
+  auto rt = root;
+  stack<TreeNode*> st;
+
+  while(rt or st.size()) {
+    while(rt) {
+      st.push(rt);
+      rt = rt->left;
+    }
+
+    rt = st.top();
+    st.pop();
+    res.emplace_back(rt->val);
+    rt = rt->right;
+  }
+
+  return res;
+}
