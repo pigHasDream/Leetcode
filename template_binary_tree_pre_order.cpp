e9ca845125532d4959f36ea44f3f42823dfa72bf
pigHasGitHub <jinmiao@utexas.edu>
2019-04-10 11:26:00 -0700

    Update template_binary_tree_pre_order.cpp

--- template_binary_tree_pre_order.cpp
+++ template_binary_tree_pre_order.cpp
@@ -1,1 +1,20 @@
 
+vector<int> preorderTraversal(TreeNode* root) {
+  vector<int> res;
+  auto rt = root;
+  stack<TreeNode*> st;
+
+  while(rt or st.size()) {
+    if(rt) {
+      res.emplace_back(rt->val);
+      if(rt->right) st.push(rt->right);
+      rt = rt->left;
+    } 
+    else {
+      rt = st.top();
+      st.pop();
+    }
+  }
+  
+  return res;
+}
