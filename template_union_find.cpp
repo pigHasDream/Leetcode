9515bfa6dabe3a9e8031da00f91673fa60c50f0a
Jin M <jinmiao@utexas.edu>
2019-04-10 23:01:48 -0700

    udpate

--- template_union_find.cpp
+++ template_union_find.cpp
@@ -1,2 +1,28 @@
 
+class UnionFindSet {
+public:
+  UnionFindSet(int n):parent_(n) {
+    for (int i = 0; i < parents_.size(); ++i)
+      parents_[i] = i;
+  }
+  
+  bool Union(int u, int v) {
+    int pu = Find(u);
+    int pv = Find(v);
+    if (pu == pv) 
+      return false;
+    
+    return true;
+  }
+  
+  int Find(int u) {        
+    // Compress the path during traversal
+    if (u != parents_[u])
+      parents_[u] = Find(parents_[u]);        
+    return parents_[u];
+  }
+
+private:
+  vector<int> parents_;
+};
 
