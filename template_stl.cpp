10f7a8d7714eaee3a08533e7b6bfb0bf15adeebb
Jin M <jinmiao@utexas.edu>
2019-04-11 00:40:31 -0700

    new stl

---
+++ template_stl.cpp
@@ -0,0 +1,23 @@
+// TODO: args, return, complexity
+
+std::lower_bound
+std::upper_bound
+std::binary_search
+std::nth_element
+std::partition
+std::stable_partition
+std::iter_swap
+std::swap
+std::is_sorted_until
+std::next_permutation
+std::prev_permutation
+std::merge
+std::inplace_merge
+std::rotate
+std::min_element
+std::max_element
+std::minmax_element
+std::accumulate
+std::fill
+std::fill_n
+std::count_if
