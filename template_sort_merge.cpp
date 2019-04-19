f07c3ee1612a7df221a4628100574fcb92b1653b
Jin M <jinmiao@utexas.edu>
2019-04-18 21:30:52 -0700

    update mergesort

--- template_sort_merge.cpp
+++ template_sort_merge.cpp
@@ -1,1 +1,13 @@
-
+template<class Iter>
+void merge_sort(Iter first, Iter last)
+{
+  // std::end - std::begin > 1 only 
+  // when more than 2 elements.
+  if (last - first > 1) {
+    Iter middle = first + (last - first) / 2;
+    merge_sort(first, middle);
+    merge_sort(middle, last);
+    std::inplace_merge(first, middle, last);
+  }
+}
+ 
