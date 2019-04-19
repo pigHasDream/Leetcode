7ab189808a03e9d61662e2b1d0e8560d9de82fce
pigHasGitHub <jinmiao@utexas.edu>
2019-04-18 21:39:35 -0700

    Update template_stl.cpp

--- template_stl.cpp
+++ template_stl.cpp
@@ -1,18 +1,30 @@
-// TODO: args, return, complexity
-
 std::lower_bound
 std::upper_bound
 std::binary_search
 std::nth_element
+
+// BidirIt partition( BidirIt first, BidirIt last, UnaryPredicate p );
+// O(N)
 std::partition
 std::stable_partition
+
 std::iter_swap
 std::swap
 std::is_sorted_until
 std::next_permutation
 std::prev_permutation
+
+// OutputIt merge( InputIt1 first1, InputIt1 last1,
+//                 InputIt2 first2, InputIt2 last2,
+//                 OutputIt d_first );
+// return Iter passes the last element inserted.
+// O(N)
 std::merge
-std::inplace_merge
+
+// void inplace_merge( BidirIt first, BidirIt middle, BidirIt last );
+// O(N) using extra memory; O(NlogN) not enough memory.
+std::inplace_merge 
+
 std::set_uion
 std::back_inserter
 std::rotate
@@ -25,4 +37,6 @@ std::fill_n
 std::count_if
 std::iota
 std::unique_ptr
-std::list // doubly-linked list
+
+// doubly-linked list
+std::list 
