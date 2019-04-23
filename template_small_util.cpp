21fcdeb05cade131977cc90ee8636e9161fc34a6
Jin M <jinmiao@utexas.edu>
2019-04-22 22:47:24 -0700

    update

--- template_small_util.cpp
+++ template_small_util.cpp
@@ -1,3 +1,4 @@
+// -----------------------------------------------------
 // Palindrome check
 bool isPalindrome(const string& s, int start, int end) {
   for(int i=start, j=end; i<j; ++i, --j)
@@ -6,9 +7,32 @@ bool isPalindrome(const string& s, int start, int end) {
   return true;
 }
 
+// -----------------------------------------------------
 // Palindrome DP transitional check condition
 // isPal[i][j]: if i~j is a palindrome
 if (s[i] == s[j] and (i-j<2 or isPal[j+1][i-1]))
   return true;
 
-// Valid parenthenese
+// -----------------------------------------------------
+// simultaneous check incremental numbers
+int nthUglyNumber(int n) {
+
+  // initially smaller size
+	vector<int> dp(1,1);
+	int i =0, j=0, k=0;
+	
+	while(dp.size() < n) {
+		
+    // gradually build up the vector
+		dp.emplace_back(min(dp[i]*2, min(dp[j]*3, dp[k]*5)));
+		
+		if(dp.back() == dp[i]*2) ++i;
+		if(dp.back() == dp[j]*3) ++j;
+		if(dp.back() == dp[k]*5) ++k;
+		
+	}
+	
+	return dp.back();
+}
+
+// -----------------------------------------------------
