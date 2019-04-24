fd937d9c359b935799c708b5f157cc78e1897b90
Jin M <jinmiao@utexas.edu>
2019-04-23 19:23:02 -0700

    udpate

--- template_tree_lca.cpp
+++ template_tree_lca.cpp
@@ -18,19 +18,18 @@ TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
 // ----------- General LCA -------------
 TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
 
-  // ASSUME: all values of nodes are unique!
-	// termination is nullptr (leaf) or root is same as
+  // ASSUME: all values of nodes are unique!	
+  // termination is nullptr (leaf) or root is same as
   // either one of the 2 targets.
 
-	if(root == nullptr or root == p or root == q) 
+  if(root == nullptr or root == p or root == q) 
     return root;
-	
-	auto left = lowestCommonAncestor(root->left, p, q);
-	auto right = lowestCommonAncestor(root->right, p, q);
-	
-	return (left and right) ? root 
-													: left ? left 
-																 : right;
+  
+  auto left = lowestCommonAncestor(root->left, p, q);
+  auto right = lowestCommonAncestor(root->right, p, q);
+  
+  return (left and right) ? root 
+                          : left ? left : right;
 }
 
 
