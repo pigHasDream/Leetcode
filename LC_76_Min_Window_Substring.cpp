b45e964bf314e4f690a83cadf429b16cf42f4513
Jin M <jinmiao@utexas.edu>
2019-04-29 21:51:34 -0700

    update

---
+++ LC_76_Min_Window_Substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  string minWindow(string s, string t) {
+    
+    vector<int> theMap(128, 0);
+    int size = s.size();
+    
+    for(const auto& c : t)  
+      ++theMap[c];
+    
+    int minLen=INT_MAX;
+    int head = INT_MAX;
+    int count = 0;
+    int left = 0;
+    
+    for(int right = 0; right < size; ++right) {
+      if(--theMap[s[right]] >= 0) ++count;
+      while(count == t.size()) {
+        if(minLen > right - left + 1) {
+          minLen = right - left + 1;
+          head = left;
+        }
+        if(++theMap[s[left]] > 0) --count;
+        ++left;
+      }
+    }
+    
+    return head == INT_MAX ? "" : s.substr(head, minLen);  
+  }
+};
