530c9d43d8f060ec88b50f8fce50a3a78dd7e315
Jin M <jinmiao@utexas.edu>
2019-05-05 16:17:37 -0700

    update

--- LC_72_Edit_Distance.cpp
+++ LC_72_Edit_Distance.cpp
@@ -33,3 +33,34 @@ public:
     
   }
 };
+
+
+// DP bottom up
+int minDistance(string s, string t) {
+
+	int size1 = s.size();
+	int size2 = t.size();
+	
+  // dp[i][j] refers to converting s[0..i] to t[0..t] count
+	vector<vector<int>> dp(size1+1, vector<int>(size2+1, 0));
+  // base case is when one of the string is empty, we will
+  // convert the full length of the other non-empty one.
+	for(int i=0; i<=size1; ++i) dp[i][0] = i;
+	for(int i=0; i<=size2; ++i) dp[0][i] = i;
+	
+	for(int i=1; i<=size1; ++i) {
+		for(int j=1; j<=size2; ++j) {
+      // if new letter the same, do nothing
+			if(s[i-1] == t[j-1])
+				dp[i][j] = dp[i-1][j-1];
+      // if not the same, it can be 
+      // replace: dp[i-1][j-1] to make i-th and j-th the same
+      // insert: use the dp[i][j-1] 
+      // delete: use the dp[i-1][j]
+			else
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+		}
+	}
+	
+	return dp[size1][size2] == 1;
+}
