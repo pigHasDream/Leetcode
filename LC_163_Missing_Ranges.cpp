128c5f33bc05f9bef7fadb558a4b96aa4e597a56
pigHasGitHub <jinmiao@utexas.edu>
2019-05-08 13:30:38 -0700

    Update LC_163_Missing_Ranges.cpp

--- LC_163_Missing_Ranges.cpp
+++ LC_163_Missing_Ranges.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
   vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
     vector<string> res;
+    // lower can be out of bounds of the nums array
+    long lo = lower;
+    // upper can also be out of bounds of the nums array
+    long up = upper;
+    
+    for(int i=0; i<nums.size(); ++i) {
+      if(lo < (long)nums[i]-1) 
+        res.emplace_back(to_string(lo) + "->" + to_string(nums[i]-1));
+      else if (lo == (long)nums[i]-1)
+        res.emplace_back(to_string(lo));
+      
+      lo = (long)nums[i]+1;
+    }
+    
+    if(lo < up) 
+      res.emplace_back(to_string(lo) + "->" + to_string(up));
+    else if(lo == up)  
+      res.emplace_back(to_string(lo));
+    
+    return res;
+  }
+  
+};
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+class Solution {
+public:
+  vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+    vector<string> res;
 
     // Using binary search to find the start index in nums
     long left = lower;
