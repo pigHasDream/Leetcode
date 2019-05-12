ad3c137d9738f1a9d33dcb6487537a76bcfd903a
Jin M <jinmiao@utexas.edu>
2019-05-11 17:49:30 -0700

    update

---
+++ LC_1000_Minimum_Cost_To_Merge_Stones.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+  int mergeStones(vector<int>& stones, int K) {
+    const int n = stones.size();
+    if ((n - 1) % (K - 1)) return -1;
+    const int kInf = 1e9;    
+    
+    vector<int> sums(n + 1);
+    for (int i = 0; i < stones.size(); ++i) sums[i + 1] = sums[i] + stones[i];
+    
+    vector<vector<int>> memo(n, vector<int>(n, -1));
+    
+    std::function<int(int,int)> doDFS;
+    doDFS = [&memo, &sums, &doDFS, n, K, kInf](int start, int end) -> int {
+      int len = end-start+1;
+      if(len<K) return 0;
+      if(memo[start][end] != -1) return memo[start][end];
+      if(start == end) return memo[start][end] = 0;
+      
+      int ret = kInf;
+      for(int m=start; m<end; m += K-1)
+        ret = min(ret, doDFS(start, m) + doDFS(m+1, end));
+      
+      if((len-1)%(K-1) == 0) ret += sums[end+1] - sums[start];
+      
+      return memo[start][end] = ret;
+    };
+    
+    return doDFS(0, n-1);
+  }
+};
