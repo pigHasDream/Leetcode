3ef713364a99bfe562d02fd4fff8b15c5a41d36e
Jin M <jinmiao@utexas.edu>
2019-05-20 00:26:58 -0700

    update

--- LC_658_Find_K_Closest_Elements.cpp
+++ LC_658_Find_K_Closest_Elements.cpp
@@ -3,6 +3,14 @@ public:
   vector<int> findClosestElements(vector<int>& arr, int k, int x) {
     int left = 0, right = arr.size()-k;
     
+    // Key thing is that we search the k range directly!
+    // now we just need to determine the starting index, then
+    // plus k elements.
+    // mid refers to the starting position.
+    // x-arr[mid] > arr[mid+k]-x means arr[mid]~arr[mid+k-1] is worse
+    // than arr[mid+1]~arr[mid+k]. Here we can ignore all elements
+    // between mid+1 ~ mid+k-1 as they are shared.
+    
     while(left < right) {
       int mid = left + (right-left)/2;
       if(x-arr[mid] > arr[mid+k]-x) {
