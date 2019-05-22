5ae3c76c97906bec5df9b0c96fd1935c98b074f9
Jin M <jinmiao@utexas.edu>
2019-05-21 23:22:09 -0700

    update

--- LC_378_Kth_Smallest_Element_In_A_Sorted_Matrix.cpp
+++ LC_378_Kth_Smallest_Element_In_A_Sorted_Matrix.cpp
@@ -20,3 +20,34 @@ public:
     return left;
   }
 };
+
+
+
+class Solution {
+public:
+  int kthSmallest(vector<vector<int>>& matrix, int k) {
+    int n = matrix.size();
+    
+    auto cmp = [&matrix](const auto &p1, const auto &p2) {
+      return matrix[p1.first][p1.second] > matrix[p2.first][p2.second];
+    };
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)> heap(cmp);
+    
+    heap.emplace(0,0);
+    while(k-- > 1 and heap.size()) {
+      auto top = heap.top();
+      heap.pop();
+      
+      int i = top.first;
+      int j = top.second;
+      
+      // This BFS based expansion is very similar to LC 373.
+      if(j<n-1) heap.emplace(i, j+1);
+      if(j==0 and i<n-1) heap.emplace(i+1, j);
+    }
+    
+    return matrix[heap.top().first][heap.top().second];
+
+  }
+};
