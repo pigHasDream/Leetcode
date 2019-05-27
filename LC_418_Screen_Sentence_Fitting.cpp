6d47f65d8ce40910a6cb8398c0827d46725abdb5
Jin M <jinmiao@utexas.edu>
2019-05-27 16:30:58 -0700

    update

--- LC_418_Screen_Sentence_Fitting.cpp
+++ LC_418_Screen_Sentence_Fitting.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
   int wordsTyping(vector<string>& sentence, int rows, int cols) {
     // s is the formatted sentence to be put to our screen
-    string s;                                          
-    for(const auto& word : sentence) s += " " + word; 
+    string s;
+    for(const auto& word : sentence) s += word + " ";
 
-    // skip the very first space char ' '
-    int start = 1;                                          
-    // advance start by one so s[start & s.length()] != ' '
-    for (int r = 0; r < rows; ++r, ++start) {               
-      // full fill current collumn, so start advance by cols
-      start += cols;                                      
-      // make sure s[start & s.length()] == ' '
-      while (s[start % s.length()] != ' ') --start;    
+    int count = 0;
+    for (int r = 0; r < rows; ++r) {
+      // full fill current collumn, so advance by cols
+      count += cols;
+      // if the ending is ' ', advance 1 more to skip the ' ' padding.
+      // although it's already fitted into one row.
+      if(s[count % s.length()] == ' ') ++count;
+      // it cannot fit completely, we need to look back to the first space.
+      else {
+        while (count > 0 and s[count % s.length()] != ' ') --count;
+        ++count; 
+      }
     }
 
-    // we began with start == 1, so (start-1) is the valid length
-    return (start-1) / s.length();                            
+    return count / s.length();
   }
 };
