15a25f5b81c93129338dc06e9a6f57831755dea8
Jin M <jinmiao@utexas.edu>
2019-06-02 17:07:25 -0700

    update

--- LC_0_560_Subarray_Sum_Equals_K.cpp
+++ LC_0_560_Subarray_Sum_Equals_K.cpp
@@ -5,10 +5,20 @@ public:
     int sum=0;
     // the memo stores <sum, count>
     unordered_map<int, int> memo;
-    memo[0] = 1;
     
-    for(int i=0; i<nums.size(); ++i) {
-      sum += nums[i];
+    // We are doing a running sum!
+    // Basically, we have to use a one-pass
+    // loop. At each number, we check its previous
+    // subarray's sum!
+    // We cannot do a 2-pass scan, as it won't
+    // correctly set the count. 
+    // prefixSum is based on its previous sums,
+    // so when count the res, we have to do it 
+    // during the scanning.
+    
+    memo[0] = 1;
+    for(const auto &n : nums) {
+      sum += n;
       res += memo[sum-k];
       ++memo[sum];
     }
