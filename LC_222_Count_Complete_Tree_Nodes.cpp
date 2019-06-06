ae9cd8cf001dbc47052de8da5044b795514dbc87
Jin M <jinmiao@utexas.edu>
2019-06-05 23:36:50 -0700

    update

--- LC_222_Count_Complete_Tree_Nodes.cpp
+++ LC_222_Count_Complete_Tree_Nodes.cpp
@@ -24,3 +24,15 @@ public:
 
   }
 };
+
+
+// Left and right subtree must be one perfect and another completed.
+// So, one of the two subtrees must have lgn run time (perfect), and 
+// only one of the two subtrees will have a recursion. 
+// Expanding this relation, we eventually get lgn*lgn
+//  
+// T(n) = T(n/2) + c1 lgn
+//      = T(n/4) + c1 lgn + c2 (lgn - 1)
+//      = ...
+//      = T(1) + c [lgn + (lgn-1) + (lgn-2) + ... + 1]
+//      = O(lgn*lgn)   
