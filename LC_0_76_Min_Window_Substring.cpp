87440a530557175cdf0c236658fe7cc3f481bd92
Jin M <jinmiao@utexas.edu>
2019-06-26 22:42:25 -0700

    update

--- LC_0_76_Min_Window_Substring.cpp
+++ LC_0_76_Min_Window_Substring.cpp
@@ -18,6 +18,14 @@ public:
     
     for(int right = 0; right < size; ++right) {
       // Note here is >= 0
+
+      // subtract s[right] still above 0, meaning an effective char.
+      // theMap[c] value refers to the count needed from T, but
+      // if S has more than needed, keep subtracting theMap[c] will
+      // lead to very negative number. Then we will use this nature 
+      // to see when moving left boundary towards right, we really still
+      // have the valid solution!
+      
       if(--theMap[s[right]] >= 0) ++count;
       while(count == t.size()) {
         if(minLen > right - left + 1) {
@@ -25,6 +33,15 @@ public:
           head = left;
         }
         // note here is > 0
+        // The magic is hash[c] value can virtually connect
+        // left and right!!
+        // if hash[c] is way below 0, meaning there are quite a few
+        // duplicated c between left and right range, so, it's ok 
+        // to keep moving left.
+      
+        // basically: right bound is to expand for more chances
+        // left bound is to test for optimality.
+
         if(++theMap[s[left]] > 0) --count;
         ++left;
       }
