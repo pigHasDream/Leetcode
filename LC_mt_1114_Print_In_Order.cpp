b231e139d4e67a7038ee7276e446c1c24402b81b
Jin M <jinmiao@utexas.edu>
2020-01-20 23:57:58 -0800

    update

--- LC_mt_1114_Print_In_Order.cpp
+++ LC_mt_1114_Print_In_Order.cpp
@@ -40,3 +40,37 @@ public:
     
   }
 };
+
+
+// Another solution with promise and future
+class Foo {
+  promise<int> p1;
+  promise<int> p2;
+  
+public:
+  Foo() {
+
+  }
+
+  void first(function<void()> printFirst) {
+
+      // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+    p1.set_value(1);
+  }
+
+  void second(function<void()> printSecond) {
+
+    p1.get_future().wait(); 
+      // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+    p2.set_value(2);
+  }
+
+  void third(function<void()> printThird) {
+
+    p2.get_future().wait();
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+  }
+};
