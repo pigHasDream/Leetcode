f3b4793f74dba0db84e1cc6f56b1efc577073369
Jin Miao <jinmiao@utexas.edu>
2020-07-30 23:50:08 -0700

    update LC delete BST node

--- template_delete_node_BST.cpp
+++ template_delete_node_BST.cpp
@@ -1,51 +1,50 @@
+#include <functional>
+struct TreeNode {
+		int val;
+		TreeNode *left;
+		TreeNode *right;
+		TreeNode() : val(0), left(nullptr), right(nullptr) {}
+		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+		TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
   TreeNode* deleteNode(TreeNode* root, int key) {
-    if(root == nullptr)
-      return nullptr;
     
-    if(root->val > key) {
-      root->left = deleteNode(root->left, key);
-    }
-    else if (root->val < key) {
+    std::function<TreeNode*(TreeNode*)> findMin = [&](TreeNode* rt) ->TreeNode* {
+      while(rt->left) {
+        rt = rt->left;
+      }
+      return rt;
+    };
+    
+    if(root == nullptr) return nullptr;
+    
+    if(key > root->val) {
       root->right = deleteNode(root->right, key);
     }
+    else if(key < root->val) {
+      root->left = deleteNode(root->left, key);
+    }
     else {
-      // case 1: leaf node;
-      if(root->left == nullptr and root->right == nullptr) {
-        delete root;
-        root = nullptr;
+      if(root->left == nullptr) {
+        auto tmp = root;
+        root = root->right;
+        delete tmp;
       }
-      // case 2: one child node;
-      else if(root->left == nullptr) {
-        TreeNode* tmpRoot = root;
-        root = root->right; 
-        delete tmpRoot;
-      }
-      else if(root->right == nullptr) {
-        TreeNode* tmpRoot = root;
+      else if (root->right == nullptr) {
+        auto tmp = root;
         root = root->left;
-        delete tmpRoot;
+        delete tmp;
       }
-      // case 3: two children nodes;
       else {
-        TreeNode* tmpRoot = findMin(root->right);
-        root->val = tmpRoot->val;
-        root->right = deleteNode(root->right, root->val);
+        auto minRight = findMin(root->right);
+        root->val = minRight->val;
+        root->right = deleteNode(root->right, minRight->val);
       }
-      
     }
     
     return root;
   }
-  
-  
-  TreeNode* findMin(TreeNode* root) {
-    TreeNode* minNode(root);
-    while(minNode->left) {
-      minNode = minNode->left; 
-    }
-    return minNode;
-  }
-  
 };
