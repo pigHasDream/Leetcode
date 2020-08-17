2a1aa75861ba3fd7cb6d0334273f229e00599411
Jin Miao <jinmiao@utexas.edu>
2020-08-16 23:13:29 -0700

    udpate

--- LC_GC_188_Best_Time_To_Buy_and_Sell_Stock_IV.cpp
+++ LC_GC_188_Best_Time_To_Buy_and_Sell_Stock_IV.cpp
@@ -13,12 +13,20 @@ public:
     
     // [0]: empty 
     // [1]: hold 
-    vector<vector<array<int, 2>>> dp(k+1, vector<array<int,2>>(prices.size(), {0,-prices.front()}));
+    // dp[j][i][0] represents the maximum profit if you have empty stock on day i after at most k - 1 transactions.
+    // dp[j][i][1] represents the maximum profit if you hold stocks on day i after at most k - 1 transactions.
+    
+    vector<vector<array<int, 2>>> dp(k+1, vector<array<int,2>>(prices.size(), {0,0}));
     
     for(int j=1; j<=k; ++j) {
-      for(int i=1; i<prices.size(); ++i) {
-        dp[j][i][0] = max(dp[j][i-1][0], dp[j][i-1][1] + prices[i]);
-        dp[j][i][1] = max(dp[j-1][i][0] - prices[i], dp[j][i-1][1]);
+      for(int i=0; i<prices.size(); ++i) {
+        if(i==0) {
+          dp[j][i][1] = -prices.front();
+        }
+        else {
+          dp[j][i][0] = max(dp[j][i-1][0], dp[j][i-1][1] + prices[i]);
+          dp[j][i][1] = max(dp[j-1][i][0] - prices[i], dp[j][i-1][1]);
+        }
       }
     }
     
