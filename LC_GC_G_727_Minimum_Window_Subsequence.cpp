aa22c06f1266ea16b0437e3c3862c28f49ed1748
Jin Miao <jinmiao@utexas.edu>
2020-09-25 00:40:39 -0700

    update

---
+++ LC_GC_G_727_Minimum_Window_Subsequence.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+  string minWindow(string S, string T) {
+    
+    int sizeS = S.size(), sizeT = T.size();
+    // use double sequence DP
+    // dp[i][j] := the min subsequence length of i-th position in S string 
+    // using i-th bit that can cover T string at j-th position!
+    S = "#"+S;
+    T = "#"+T;
+    
+    vector<vector<int>> dp(sizeS+1, vector<int>(sizeT+1, 0));
+    const int inf = 1e8;
+    
+		// the boundary condition is checked through the subscription
+    // of dp[i-1][j-1] in the loop!
+    
+    for(int i=1; i<=sizeT; ++i)
+      dp[0][i] = inf;
+    
+    for(int i=1; i<=sizeS; ++i) {
+      for(int j=1; j<=sizeT; ++j) {
+        if(S[i]==T[j]) {
+          dp[i][j] = dp[i-1][j-1] + 1;
+        }
+        else {
+          dp[i][j] = dp[i-1][j] + 1;
+        }
+      }
+    }
+    
+    int minLen = inf, pos = -1;
+    for(int i=1; i<=sizeS; ++i) {
+      if(dp[i][sizeT] < minLen) {
+        minLen = dp[i][sizeT];
+        pos = i;
+      }
+    }
+    
+    if(minLen >= inf) 
+      return "";
+    else 
+      return S.substr(pos-minLen+1, minLen);
+    
+  }
+};
